a444e4ce6b8d75fd8fb7af1f8f44e2a35487ec6b
Vigneshwaran110203 <vigneshwaranvimal17@gmail.com>
2025-04-29 10:26:18 +0530

    implement the employee management

---
+++ middlewares/auth_middleware.go
@@ -0,0 +1,37 @@
+package middlewares
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+)
+
+func AuthMiddleware() gin.HandlerFunc {
+	return func(ctx *gin.Context) {
+		tokenString, err := ctx.Cookie("crm_token")
+		if err != nil || tokenString == "" {
+			ctx.JSON(http.StatusUnauthorized, gin.H{
+				"error": "Unauthorised: No token provided",
+			})
+			ctx.Abort()
+			return
+		}
+
+		token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, jwt.ErrInvalidKey
+			}
+			return []byte("gh8fho1u7ba9"), nil
+		})
+
+		if err != nil || !token.Valid {
+			ctx.JSON(http.StatusUnauthorized, gin.H{
+				"error": "unauthorised: invalid token",
+			})
+			ctx.Abort()
+			return
+		}
+		ctx.Next()
+	}
+}
--- routes/employee_route.go
+++ routes/employee_route.go
@@ -2,18 +2,21 @@ package routes
 
 import (
 	"prod-crm/controllers"
+	"prod-crm/middlewares"
 
 	"github.com/gin-gonic/gin"
 	"gorm.io/gorm"
 )
 
-func EmployeeRoutes(r *gin.Engine, db *gorm.DB){
+func EmployeeRoutes(r *gin.Engine, db *gorm.DB) {
 	employeeHandler := controllers.NewEmployeeHandler(db)
 	employeeGroup := r.Group("/api/employees")
-
-	employeeGroup.POST("/", employeeHandler.AddEmployee)
-	employeeGroup.GET("/", employeeHandler.GetEmployees)
-	employeeGroup.GET("/:id", employeeHandler.GetEmployeeByID)
-	employeeGroup.PUT("/:id", employeeHandler.UpdateEmployee)
-	employeeGroup.DELETE("/:id", employeeHandler.DeleteEmployee)
+	employeeGroup.Use(middlewares.AuthMiddleware())
+	{
+		employeeGroup.POST("/", employeeHandler.AddEmployee)
+		employeeGroup.GET("/", employeeHandler.GetEmployees)
+		employeeGroup.GET("/:id", employeeHandler.GetEmployeeByID)
+		employeeGroup.PUT("/:id", employeeHandler.UpdateEmployee)
+		employeeGroup.DELETE("/:id", employeeHandler.DeleteEmployee)
+	}
 }
